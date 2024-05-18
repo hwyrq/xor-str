de30fe3099ab909be3f7f12c469c9f802c5c6820
wcz <hwyrqw@163.com>
2024-05-18 16:58:55 +0800

    README.md

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,40 @@
 
-/// # XOR 编译期加密字符串并且运行时自动解密
-/// # XOR encrypts strings at compile time and decrypts them automatically at run time
-///
-/// 为什么要用这个？
-///
-/// 原因：项目编译成机器码后，数据库密码或者sql语句等敏感信息，是暴露在机器码中的，
-///
-/// 如果通过gbk编码强行打开该exe文件，通过搜索"mysql"关键字，即可看到数据库链接信息，包括您的密码
-///
-/// 通过使用该依赖，就可以隐藏重要文本数据
-///
-/// Why use this?
-///
-/// Reason: After the project is compiled into machine code, sensitive information such as database passwords or sql statements are exposed to the machine code
-///
-/// If you force open the exe file with gbk encoding, you can see the database link information, including your password, by searching for the keyword "mysql"
-///
-/// By using this dependency, you can hide important text data
-///
-/// # 使用方式 how to use
-/// ```
-/// use xor_str::xor;
-/// let value = xor!("你好 世界！");
-/// println!("{}",value);
-/// ```
-///定义xor宏
+//! # XOR 编译期加密字符串并且运行时自动解密
+//!
+//! # XOR encrypts strings at compile time and decrypts them automatically at run time
+//!
+//! 为什么要用这个？
+//!
+//! 原因：项目编译成机器码后，数据库密码或者sql语句等敏感信息，是暴露在机器码中的，
+//!
+//! 如果通过gbk编码强行打开该exe文件，通过搜索"mysql"关键字，即可看到数据库链接信息，包括您的密码
+//!
+//! 通过使用该依赖，就可以隐藏重要文本数据
+//!
+//! Why use this?
+//!
+//! Reason: After the project is compiled into machine code, sensitive information such as database passwords or sql statements are exposed to the machine code
+//!
+//! If you force open the exe file with gbk encoding, you can see the database link information, including your password, by searching for the keyword "mysql"
+//!
+//! By using this dependency, you can hide important text data
+//! # 使用方式 how to use
+//! ```
+//! [dependencies]
+//!  xor-str = "*"
+//!
+//! use xor_str::xor;
+//! use xor_str::encode;
+//! use xor_str::decode;
+//! fn main() {
+//!     println!("{}",xor!("Hello, world!"));
+//! }
+//! ```
+pub use xor_str_encode::encode;
+
+///xor宏
 /// 在这里加密并解密，加密是过程宏，编译期间执行
+
 #[macro_export]
 macro_rules!  xor {
     ($str:literal) => {
@@ -46,8 +55,8 @@ pub fn decode(str: &[u8]) -> String {
 ///创建并通过xor方式加密字符串
 #[cfg(test)]
 mod tests {
-    use xor_str_encode::encode;
     use crate::decode;
+    use crate::encode;
     #[test]
     fn it_works() {
         println!("{}", xor!(br#"hello word!"#));
--- xor-str-encode/src/lib.rs
+++ xor-str-encode/src/lib.rs
@@ -1,11 +1,44 @@
+
+//! # XOR 编译期加密字符串并且运行时自动解密
+//!
+//! # XOR encrypts strings at compile time and decrypts them automatically at run time
+//!
+//! 为什么要用这个？
+//!
+//! 原因：项目编译成机器码后，数据库密码或者sql语句等敏感信息，是暴露在机器码中的，
+//!
+//! 如果通过gbk编码强行打开该exe文件，通过搜索"mysql"关键字，即可看到数据库链接信息，包括您的密码
+//!
+//! 通过使用该依赖，就可以隐藏重要文本数据
+//!
+//! Why use this?
+//!
+//! Reason: After the project is compiled into machine code, sensitive information such as database passwords or sql statements are exposed to the machine code
+//!
+//! If you force open the exe file with gbk encoding, you can see the database link information, including your password, by searching for the keyword "mysql"
+//!
+//! By using this dependency, you can hide important text data
+//! # 使用方式 how to use
+//! ```
+//! [dependencies]
+//!  xor-str = "*"
+//!
+//! use xor_str::xor;
+//! use xor_str::encode;
+//! use xor_str::decode;
+//! fn main() {
+//!     println!("{}",xor!("Hello, world!"));
+//! }
+//! ```
+
+
 ///具体的xor加密代码都在这里实现
 use std::ops::Index;
 use syn::{Expr, Lit, parse_macro_input, Token};
-use syn::__private::{IntoSpans, Span, TokenStream, ToTokens};
+use syn::__private::{ Span, TokenStream, ToTokens};
 use syn::__private::quote::quote;
 use syn::parse::Parser;
 use syn::punctuated::Punctuated;
-use syn::spanned::Spanned;
 
 #[proc_macro]
 pub fn encode(input: TokenStream) -> TokenStream {
